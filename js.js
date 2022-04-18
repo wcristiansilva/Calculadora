c5790d0ac48ce761df00fd3fdea64dbf2eea1a27
Willian Cristian <44729804+wcristiansilva@users.noreply.github.com>
2022-04-17 23:03:45 -0300

    .

--- js.js
+++ js.js
@@ -1,46 +1,29 @@
-/* function insert(params) {
-    resultado.innerHTML += params 
-}
-
-function clean(){
-    resultado.innerHTML = ''
-}
-function back() {
-    if (resultado.textContent) {
-        let result = document.getElementById('resultado').innerHTML
-        resultado.innerHTML = result.substring(0, result.length - 1)
-    }
-}
-
-function calcula() {
-    if(resultado.textContent != 'Erro'){
-        document.getElementById('resultado').innerHTML = eval(resultado.innerHTML)
-    }
-} */
+const resultado = document.getElementById('resultado');
 
 function Calculator(params) {
-    var result = params;
-    if (result !== undefined && result !== null && result !== '' && result !== 'Erro') {
-    }
-    if (result == '+' || result == '-' || result == '*' || result == '/' || result == '.' || result == '1' || 
-    result == '2' || result == '3' || result == '4' || result == '5' || result == '6' || result == '7' || result == '8' || result == '9' || result == '0' ){
-    resultado.innerHTML += result;
+
+    if (params == '+' || params == '-' || params == '*' || params == '/' || params == '.' || params == '1' || params == '2' ||
+        params == '3' || params == '4' || params == '5' || params == '6' || params == '7' || params == '8' || params == '9' || params == '0') {
+
+        resultado.innerHTML += params;
     }
 
-    if (result == 'C'){
+    if (params == 'C') {
+
         resultado.innerHTML = '';
     }
-    
-    if (result == '<'){
+
+    if (params == '<') {
+
         let result = document.getElementById('resultado').innerHTML;
         resultado.innerHTML = result.substring(0, result.length - 1);
     }
 
-    if (result == '='){
-        if(resultado.textContent != 'Erro'){
+    if (params == '=') {
+
+        if (resultado !== '') {
             document.getElementById('resultado').innerHTML = eval(resultado.innerHTML);
         }
     }
 
-    
 }
